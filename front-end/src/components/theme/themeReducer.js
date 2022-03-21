ed6e0656ebfe49f64d9d31ddd4556ad5f2ac2fd6
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2022-03-20 20:32:22 -0500

    Adding theme reducer

--- front-end/src/components/theme/themeReducer.js
+++ front-end/src/components/theme/themeReducer.js
@@ -1,5 +1,7 @@
 import { types } from 'components/types/types';
 
+export const themeInitialState = { isDarkMode: false };
+
 const themeReducer = (state = {}, action) => {
     switch (action.type) {
         case types.darkMode:
